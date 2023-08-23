ec13c316a3b7dc171f4475ec6a9ddd7818e27479
elhamdaoui <elhamdaouimar1@gmail.com>
2023-08-23 21:16:34 +0100

    5 added

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * @n: number
+ * Return: 1 if positive
+ * 0 if zero
+ * otherwise -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar((char) 48);
+		return (0);
+	}
+	else
+	{
+		_putchar("-");
+		return (-1);
+	}
+}
