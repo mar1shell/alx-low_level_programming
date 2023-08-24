44c0ed8787335531afcc4dcdf4ecba26bf1626ce
elhamdaoui <elhamdaouimar1@gmail.com>
2023-08-24 18:23:21 +0100

    102 added

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long i, j, sum, m;
+
+	i = 1;
+	j = 2;
+	m = 2;
+	printf("%lu, %lu, ", i, j);
+	while (m < 59)
+	{
+		sum = i + j;
+		i = j;
+		j = sum;
+		printf("%lu, ", j);
+		m++;
+	}
+	sum = i + j;
+	printf("%lu\n", sum);
+}
