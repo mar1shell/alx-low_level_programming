d12431e679c626f14f4e59b7b931fadbff4c42f5
elhamdaoui <elhamdaouimar1@gmail.com>
2023-08-24 16:36:44 +0100

    10 fixed

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,25 +4,12 @@
  * add - entry point
  * @n: number
  * @m: number
- * Return: 0
+ * Return: a
  */
 
 int add(int n, int m)
 {
 	int a;
 
-	a = n + m;
-	if (a % 10 == 0)
-		_putchar(a + '0');
-	else if (a % 100 == 0)
-	{
-		_putchar((a / 10) + '0');
-		_putchar((a % 10) + '0');
-	}
-	else
-	{
-		_putchar((a / 100) + '0');
-		_putchar(((a % 100) - (a % 10)) + '0');
-		_putchar((a % 10) + '0');
-	}
+	return (a);
 }
