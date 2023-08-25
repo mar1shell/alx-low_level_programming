dbe79bf8e4e21e43cd2a6637683f7afc13a9cf72
elhamdaoui <elhamdaouimar1@gmail.com>
2023-08-25 15:37:11 +0100

    3 added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	 while (i < 10)
+	 {
+		 _putchar(i + '0');
+		 i++;
+	 }
+	 _putchar((char) 10);
+}
