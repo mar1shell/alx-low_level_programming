c20cb40cbac947004972b4e4b8c1e9cb4d1ea778
elhamdaoui <elhamdaouimar1@gmail.com>
2023-08-25 16:25:20 +0100

    5 added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i, j, k;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		k = 0;
+		while (j < 15)
+		{
+			if (j > 9)
+			{
+				k -= 10;
+				_putchar('1');
+			}
+			_putchar(k + '0');
+			j++;
+			k++;
+		}
+		i++;
+		_putchar((char) 10);
+	}
+}
+
