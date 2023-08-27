150953c61ddb7ea72b7e0ab4deb62e044ac83a26
elhamdaoui <elhamdaouimar1@gmail.com>
2023-08-27 17:19:20 +0100

    2 added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strlen - function that return the length
+ * of a string
+ * @s: string
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int i, sum;
+
+	i = 0;
+	sum = 0;
+	while (true)
+	{
+		if (*(p + i) != (char) 0)
+		{
+			sum++;
+			i++;
+		}
+		else
+			break;
+	}
+	return (sum);
+}
