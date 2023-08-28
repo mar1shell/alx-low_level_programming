7f6dfcceaab9c71bf1cfa5eca5e88e798df28e0e
elhamdaoui <elhamdaouimar1@gmail.com>
2023-08-28 14:10:57 +0100

    6added

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * puts2 - function to print odd indexed chrachters
+ * in a string
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int c, i;
+
+	while (1 > 0)
+	{
+		if (*(str + i) != (char) 0)
+		{
+			c++;
+			i++;
+		}
+		else
+			break;
+	}
+	c--;
+	for (i = 0; i <= c; i + 2)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar((char) 10);
+}
