2b550646f7ee005681e08d80ed1c463f92f8d470
mar1shell <elhamdaoui mar1@gmail.com>
2023-09-05 12:01:31 +0100

    0 fixed 2nd

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -13,7 +13,9 @@ void _puts_recursion(char *s)
 	i = 0;
 	if (*(s + i) == (char)  0)
 		return;
-	_putchar((s + i));
+	
+	_putchar(*(s + i));
 	i++;
-	_puts_recursion(*(s + i));
+
+	_puts_recursion(s + i);
 }
