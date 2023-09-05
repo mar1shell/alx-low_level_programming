b6418b70c64c3a00a1cbaf905f5d8cd82e6919e9
mar1shell <elhamdaoui mar1@gmail.com>
2023-09-05 11:39:44 +0100

    README.md added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (*(s + i) == (char)  0)
+		return;
+	_putchar(*(s + i));
+	i++;
+	_puts_recursion(*(s + i));
+}
