59704da014ab62241bab0db5b96d87c3d9e28ee0
mar1shell <elhamdaoui mar1@gmail.com>
2023-09-07 10:00:44 +0100

    2 refixed

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,20 +1,15 @@
 #include "main.h"
-
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string
  * Return: length of s
  */
-
-int n = 0;
-
-int i = 0;
-
 int _strlen_recursion(char *s)
 {
+	int n;
+
 	if (*s == (char) 0)
-		return n;
-	n++;
-	i++;
-	_strlen_recursion(s + i);
+		return (0);
+	n = 1 + _strlen_recursion(s + 1);
+	return (n);
 }
