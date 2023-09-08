2ca3a2111df487877b11d3884fd2f3b73b39bc38
mar1shell <elhamdaoui mar1@gmail.com>
2023-09-08 17:33:16 +0100

    101 added

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,28 +1,24 @@
 #include "main.h"
+int cmp(char *s1, char *s2, int i, int j) {
+    // Base case: If both strings reach the end, it's a match
+    if (s1[i] == '\0' && s2[j] == '\0') {
+        return 1;
+    }
 
-/**
- *
- */
+    // If s2[j] is an asterisk, skip consecutive asterisks
+    while (s2[j] == '*') {
+        j++;
+    }
 
-int cmp(char *s1, char *s2, int i, int j)
-{
-	if (*(s1 + i) != '\0') 
-		return 0;
-	if (*(s2 + j) == '*')
-	{
-		while (*(s2 + j) == '*')
-		{
-			j++;
-		}	
-		while (*(s1 + i) != '\0') 
-		{
-			return (cmp(s1, s2, i++, j)); 
-		}
-	}
-	if (*(s1 + i) == *(s2 + j))
-		return (cmp(s1, s2, i + 1, j+1));
+    // If s1[i] matches s2[j], continue the comparison
+    if (s1[i] == s2[j]) {
+        return cmp(s1, s2, i + 1, j + 1);
+    }
+
+    // No match, return 0
+    return 0;
 }
-int wildcmp(char *s1, char *s2)
-{
-	return (cmp(s1, s2, 0, 0));
+
+int wildcmp(char *s1, char *s2) {
+    return cmp(s1, s2, 0, 0);
 }
