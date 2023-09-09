afa4baf2e270806854cd4a276f912ad00952f1f0
mar1shell <elhamdaoui mar1@gmail.com>
2023-09-09 12:16:35 +0100

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: string
+ * @src: string
+ * @n: int
+ * Return: the concatenated string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+		ptr++;
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	*ptr = '\0';
+
+	return (dest);
+}
