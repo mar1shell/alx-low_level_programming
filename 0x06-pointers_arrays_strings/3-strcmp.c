53f74ba471f3237c404402f1cbbe4820366dc181
mar1shell <elhamdaoui mar1@gmail.com>
2023-09-09 14:37:47 +0100

    3 fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
 /**
+ * _strcmp - compares two string
+ * @s1: string
+ * @s2: string
+ * Return: 13 if s1 > S2; -13 if s1 < s2; otherwise 0
  */
 
 int _strcmp(char *s1, char *s2)
@@ -8,15 +12,15 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (*s1 < *s2)
-			return (-15);
+			return (-13);
 		if (*s1 > *s2)
-			return (15);
+			return (13);
 		s1++;
 		s2++;
 	}
 	if (*s1 < *s2)
-		return (-15);
+		return (-13);
 	if (*s1 > *s2)
-		return (15);
+		return (13);
 	return (0);
 }
