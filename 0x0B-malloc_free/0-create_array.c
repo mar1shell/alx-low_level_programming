76f171ff57295ddf0d8e5b18b91421042c2d55f3
mar1shell <elhamdaoui mar1@gmail.com>
2023-09-10 13:27:57 +0100

    0 rerefixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,12 @@ char *create_array(unsigned int size, char c)
 	char *s = NULL;
 
 	unsigned int i;
+	
 	if (!size)
 		return (NULL);
-	s = malloc(sizeof(int) * size);
+	s = malloc(sizeof(char) * size);
+	if (s == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		s[i] = c;
 	return (s);
