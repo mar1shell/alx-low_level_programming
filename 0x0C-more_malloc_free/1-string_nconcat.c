c39eabfdbc7244532412c505024136d7460e80cd
mar1shell <elhamdaoui mar1@gmail.com>
2023-09-11 15:33:07 +0100

    1 added

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.H>
+
+/**
+ * _strlen - determinbes the length of a string
+ * @s: string
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenets n bytes from a string to another
+ * and stores them in a new string
+ * @s1: string
+ * @s2: string
+ * @n: unsigned int number of bytes
+ * Return: NULL if error; concatenated string cs
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *cs = NULL;
+
+	int i, j, len, len1, len2;
+
+	len1 = (s1 == NULL) ? 0 : _strlen(s1);
+	len2 = (s2 == NULL) ? 0 : _strlen(s2);
+	len2 = (n < len2) ? n : len2;
+	cs = malloc(sizeof(char) * (len + 1));
+	if (cs == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		*(cs + i) = *(s1 + i);
+	for (j = 0; j < n && j < len2; j++)
+	{
+		*(cs + i) = *(s2 + j);
+			i++;
+	}
+	*(cs + i) = '\0';
+	return (cs);
+}
