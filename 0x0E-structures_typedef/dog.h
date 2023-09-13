6a0b5da98a55302a4fcdc58e63c8461e8a18067f
mar1shell <elhamdaoui mar1@gmail.com>
2023-09-13 17:27:20 +0100

    0 added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog name, age and owner
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ *
+ * Description: dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
