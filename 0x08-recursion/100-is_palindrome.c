e09db723c35a720bacc90ab7db1dbb64e5599f05
mar1shell <elhamdaoui mar1@gmail.com>
2023-09-16 10:09:56 +0100

    100 fixed

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,68 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 #include <string.h>
 
-int is_palindrome(char *s);
 
-int is_alphanumeric(char c) {
-    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
-}
-
-int recursive_palindrome(char *s, int start, int end) {
-    if (start >= end) {
-        return 1;
-    }
-
-    if (s[start] != s[end]) {
-        return 0;
-    }
-
-    return recursive_palindrome(s, start + 1, end - 1);
-}
-
-int is_palindrome(char *s) {
+int is_palindrome(char *s)
+{
     int length = strlen(s);
-    if (length <= 1) {
-        return 1;
-    }
+    int i;
 
-    int start;
-
-    int end;
-
-    end = length - 1;
-
-    start = 0;
-    while (start < end) {
-        while (start < length && !is_alphanumeric(s[start])) {
-            start++;
-        }
-        while (end >= 0 && !is_alphanumeric(s[end])) {
-            end--;
-        }
-
-        if (start < length && end >= 0) {
-            if (s[start] != s[end]) {
-                return 0;
-            }
-            start++;
-            end--;
+    for (i = 0; i < length / 2; i++) {
+        if (s[i] != s[length - 1 - i]) {
+            return 0; // Not a palindrome
         }
     }
 
-    return 1;
-}
-
-int main(void) {
-    int r;
-
-    r = is_palindrome("level");
-    printf("%d\n", r);
-    r = is_palindrome("redder");
-    printf("%d\n", r);
-    r = is_palindrome("test");
-    printf("%d\n", r);
-    r = is_palindrome("step on no pets");
-    printf("%d\n", r);
-    return 0;
+    return 1; // Palindrome
 }
 
